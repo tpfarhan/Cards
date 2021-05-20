a9500af238c2a34ccedc8a72be45c04b9c5ec4fc
farhan <tpfarhan007@gmail.com>
2021-05-20 12:18:53 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts = [
+    {
+      title : 'Neat Tree',
+      imageUrl : 'assets/tree.jpeg',
+      username : 'nature',
+      content : 'I saw this neat tree today'
+    },
+    {
+      title : 'Snowy mountain',
+      imageUrl : 'assets/mountain.jpeg',
+      username : 'nmountainlover',
+      content : 'I saw this snowy mountain today'
+    },
+    {
+      title : 'Biking',
+      imageUrl : 'assets/biking.jpeg',
+      username : 'biker',
+      content : 'I love biking'
+    },
+    
+  ]
 }
